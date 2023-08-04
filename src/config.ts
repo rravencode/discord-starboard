98b89990002cc0549649fa76ad0f67a860dada7c
Roman <108236042+romanwashere@users.noreply.github.com>
2023-08-04 12:55:31 +0300

    Update config.ts
    fix

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export default {
     discord: {
-        token: 'MTEwMjU0NDQ3Mzg5ODA5ODY5OA.Gueinp.qXpRv7jQm8CDD6_yH8WJIHq0h3ruZmq69TJGuY',
-        id: '1102544473898098698',
-        emojiId: '1134462776186781748',
-        channelId: '1126455931660681267',
+        token: '',
+        id: '',
+        emojiId: '',
+        channelId: '',
     },
 };
